05f366635a24892c2f2461be977f0f9a82a5d510
Hamza Waheed <hamzawaheed104@gmail.com>
2023-12-06 14:00:49 +0500

    Query scoping, CRUD, UI tailwind, Rate limiting, Cache implementation

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['review','rating'];
 
     public function book(){
         return $this->belongsTo(Book::class);
@@ -16,4 +19,17 @@ class Review extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    protected static function booted()
+    {
+        static::updated(
+            fn(Review $review) => Cache::forget('book:'.$review->book_id)
+        );
+        static::deleted(
+            fn(Review $review) => Cache::forget('book:'.$review->book_id)
+        );
+        static::created(
+            fn(Review $review) => Cache::forget('book:'.$review->book_id)
+        );
+    } 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('books', BookController::class)->only(['index', 'show']);
+    Route::resource('books.reviews', ReviewController::class)->scoped(['review' => 'book'])->only(['create','store']);
 });
 
 require __DIR__.'/auth.php';
